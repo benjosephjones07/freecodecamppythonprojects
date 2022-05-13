4baa6cad42714b2c7b2ec6321ac7836b3ca4ac52
benjosephjones07 <benjosephjones07@gmail.com>
2022-05-13 06:33:06 +0000

    madlib script almost done

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+# youtuber = "Kylie Ying"
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)

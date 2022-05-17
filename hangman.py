085067168e7d21e9b9aac5f93bdcac2b1de200d6
benjosephjones07 <benjosephjones07@gmail.com>
2022-05-17 10:30:40 +0000

    complete hangman

--- hangman.py
+++ hangman.py
@@ -15,18 +15,36 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    lives = 6
+
     #getting user input
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0 and lives > 0 :
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please guess again.')
+        #letters used
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+            else:
+                lives = lives - 1
+                print('Letter is not in word')
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please guess again.')
+
+        else:
+            print('You did not use a valid character. Please try again.')
+
+    if lives == 0:
+        print('You died! Sorry, the word was', word)
     else:
-        print('You did not use a valid character. Please try again.')
+        print('You guessed the word', word, '!!')
 
-user_input = input('Type something: ')
-print(user_input)
+hangman()
